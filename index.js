575ff8c28e67b659219c1f2183ded5a66b3adf3d
Josimar <josimarhenriquedeoliveira@gmail.com>
2022-12-08 18:42:37 -0300

    camiseta jesus

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function alerta(){ 
+    alert("Produto adicionado ao carrinho");
+}
+
+function img01 (){
+    document.getElementById("imagemcamisetajesus").src="ImagensSublimoon/camisetaJesus.png";
+}
+function img02 (){
+    document.getElementById("imagemcamisetajesus").src="ImagensSublimoon/camiseta_jesus1.png";
+}
+function img03 (){
+    document.getElementById("imagemcamisetajesus").src="ImagensSublimoon/camiseta_jesus2.png";
+}
